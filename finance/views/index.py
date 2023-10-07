2a4ff18dc73480a57961d9229c34abc52337d0a9
Mick Ohtake <mikado98765@gmail.com>
2023-10-07 15:18:10 -0500

    Addding more test data, updating index and model

--- finance/views/index.py
+++ finance/views/index.py
@@ -11,5 +11,19 @@ import finance
 @finance.app.route('/')
 def show_index():
     """Display / route."""
-    context = {}
+
+     # Connect to database
+    connection = finance.model.get_db()
+
+    # Query database
+    cur = connection.execute(
+        "SELECT * "
+        "FROM transactions"
+    )
+    trnz = cur.fetchall()
+
+    balance=0
+    for trn in trnz:
+        balance += trn["amount"]
+    context = {"trnz": trnz, "balance": balance}
     return flask.render_template("index.html", **context)
--- sql/data.sql
+++ sql/data.sql
@@ -3,6 +3,9 @@ PRAGMA foreign_keys = ON;
 INSERT INTO transactions(id, amount, paymentID, purchaseID, companyID, notes)
 VALUES (0, -1000, 0, 0, 0, "Initial balance");
 
+INSERT INTO transactions(id, amount, paymentID, purchaseID, companyID, notes)
+VALUES (1, 52234.66, 0, 0, 0, "Gettin rich");
+
 INSERT INTO payments(id, name)
 VALUES(0, "ADMIN");
 
